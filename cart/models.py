d849dafa81f29d669c92b9c6713dd85e283ce41c
MahogaHeryasa <hery.heryasa@gmail.com>
2023-10-26 22:10:45 +0700

    add login,register,logout + cart

--- cart/models.py
+++ cart/models.py
@@ -3,3 +3,7 @@ from book.models import Book
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book_amount = models.IntegerField()
