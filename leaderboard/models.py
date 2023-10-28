ee2bce59f05af4d237496e08428ba5ca322d87f2
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2023-10-28 22:41:36 +0700

    add model

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from book.models import Book
+from django.contrib.auth.models import User
 
-# Create your models here.
+class LeaderBoard(models.Model):
+    users = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    # quote = models.TextField(default = "")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, default=None)
--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -1,13 +1,13 @@
 from django import views
 from django.urls import path
-from .views import leaderboard, get_product_json, search_bar
+from .views import  show_leaderboard, get_product_json, search_bar
 
 
 app_name = 'leaderboard'
 
 urlpatterns = [
     # path('', show_leaderboard),
-    path('', leaderboard),
+    path('', show_leaderboard, name='show_leaderboard'),
     path('get_product_json/', get_product_json, name='get_product_json'),
     path('search_bar/<str:value>', search_bar, name='search_bar'),
 ]
