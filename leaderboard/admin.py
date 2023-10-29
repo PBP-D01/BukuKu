ae3259b2ddb838b4144efe312ab40740dfcef6e1
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2023-10-29 13:55:08 +0700

    update for comment section

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import LeaderBoard
+from .models import LeaderBoard, Comment
 # Register your models here.
 
 admin.site.register(LeaderBoard)
+admin.site.register(Comment)
