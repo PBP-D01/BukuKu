58ca6487cc84338aa348d8a7e8079f1f55c9f98f
kayzaa nuur <kayza.azura@gmail.com>
2023-10-29 14:37:23 +0700

    add product_page

--- product_page/urls.py
+++ product_page/urls.py
@@ -6,7 +6,7 @@ app_name = 'product_page'
 urlpatterns = [
     path('', show_product, name='show_product'),
     path('get_product_json/', get_product_json, name='get_product_json'),
-    path('add_to_cart/<int:id>', add_cart, name='add_cart'),
+    path('add_to_cart', add_cart, name='add_cart'),
     path('search_bar/<str:value>', search_bar, name='search_bar'),
+    path('search_bar2/<str:value>', search_bar2, name='search_bar2'),
 ]
-
