1371cf959204f2adb218c8f082751e43002c2a21
brianjo25 <brian.jonathan@ui.ac.id>
2023-12-20 22:23:13 +0700

    Perbaikan url

--- review/urls.py
+++ review/urls.py
@@ -6,4 +6,6 @@ app_name = 'review'
 urlpatterns = [
     path('<str:book_id>/', views.review_list, name='review_list'),
     path('<str:book_id>/add/', views.add_review, name='add_review'),
+    path('api/<str:book_id>/get/', views.get_review_api, name='get_review_api'),
+    path('api/<str:book_id>/post/review/', views.post_review_api, name='post_review_api'),
 ]
